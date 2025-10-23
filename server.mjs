9f504cb948d7f198a6d3f3b1434b3928356476a8
Jakub Honzal <1jakubhonzal@gmail.com>
2025-10-23 12:50:35 +0200

    server changes

--- server.mjs
+++ server.mjs
@@ -14,6 +14,6 @@ app.use(serve);
 
 // Railway používá PORT z env proměnné
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
